cdb9836c8bda48b9b97359319f65ee0c57f2b5ed
Vinícius Botelho <68855615+viniciusdev-br@users.noreply.github.com>
2023-06-29 14:36:08 -0300

    add: set gui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 import * as THREE from 'three';
+import { GUI } from 'dat.gui';
 
 import SceneInit from './lib/SceneInit';
 
@@ -10,11 +11,23 @@ function App() {
     test.initialize();
     test.animate();
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxGeometry = new THREE.BoxGeometry(24, 24, 24);
     const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 
     test.scene.add(boxMesh);
+
+    // Inicializa GUI
+    const gui = new GUI();
+
+    // PART 2 - Changing Geometry (scale, rotation)
+    gui.add(boxMesh.rotation, 'x', 0, Math.PI).name('Rotate X Axis');
+    gui.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
+    gui.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
+    gui.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
+    gui.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
+    gui.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
+
   }, []);
 
   return (
